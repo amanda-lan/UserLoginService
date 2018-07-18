c1e7e6d86755a14474fb0a41c198897242b43cd4
Amanda LAN <amandalan@Amandas-MacBook-Pro.local>
2018-07-18 11:15:46 +1000

    error

--- www/insert.php
+++ www/insert.php
@@ -2,39 +2,37 @@
 
 include "index.php";
 
-//phpinfo();
-$servername = "localhost";
-$username = "root";
-$password = "lw0105~LW";
+$dbusername = "root";
+$dbpassword = "lw0105~LW";
+
+$dsn = "mysql:host=localhost;dbname=UserLoginDB;charset=utf8mb4";
+$options = [
+  PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=UserLoginDB", $username, $password);
-    // set the PDO error mode to exception
+    $conn = new PDO($dsn, $dbusername, $dbpassword，$options);
+    /* set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //$conn->exec($sql);
-    echo "Connection successfully"; 
-    }
-catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-    }
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is comming from a form
+    $conn->exec($sql);*/
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is comming from a form
     
     $u_name = filter_var($_POST["Username"], FILTER_SANITIZE_STRING); //set PHP variables like this so we can use them anywhere in code below
-    $u_Password = filter_var($_POST["Password"], FILTER_SANITIZE_EMAIL);
-    $u_CPassword = filter_var($_POST["ConfirmPassword"], FILTER_SANITIZE_STRING);
+    $u_password = filter_var($_POST["Password"], FILTER_SANITIZE_EMAIL);
+    $u_cpassword = filter_var($_POST["ConfirmPassword"], FILTER_SANITIZE_STRING);
 
     if (empty($u_name)){
         die("Please enter your username");
     }
-    if (empty($u_Password)){
+    if (empty($u_password)){
         die("Please enter your password");
     }
-        
-    if (empty($u_CPassword)){
+    if (empty($u_cpassword)){
         die("Please confirm your password");
     } 
-    if ($u_Password != $u_CPassword){
+    if ($u_password != $u_cpassword){
         die("Your password does not match, please enter again");
     }    
     
@@ -43,18 +41,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is comming from a form
     /*$statement->bind_param('sss', $u_name, $u_Password, $u_CPassword); //bind values and execute insert query*/
 
     $statement->bindParam(':u_name', $u_name);
-    $statement->bindParam(':u_Password', $u_Password);
-    $statement->bindParam(':u_CPassword', $u_CPassword);
-
+    $statement->bindParam(':u_Password', $u_password);
+    $statement->bindParam(':u_CPassword', $u_cpassword);
+    $statement->execute()；
     
-    if($statement->execute()){
-        print "Hello " . $u_name. "!, your message has been saved!";
+    /*if(){
+        echo "Hello " . $u_name. "!, your message has been saved!";
     }else{
-        print $mysqli->error; //show mysql error if any
+        echo $mysqli->error; //show mysql error if any
+    }*/
     }
-}
-
-
-
+    echo "Connection successfully"; 
+    }
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+    }
 
 ?>
